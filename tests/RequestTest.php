078e20faa8d6418c7840b346ac1bc8a7ab73c09b
Jeppe Christiansen <jeppe@codr.dk>
2015-02-17 23:39:25 +0100

    Added method for request with and without redirects

--- tests/RequestTest.php
+++ tests/RequestTest.php
@@ -73,23 +73,25 @@ class RequestTest extends PHPUnit_Framework_TestCase {
     /** @test */
     public function it_can_retrieve_http_information() {
         $request = $this->getMockBuilder('Jeppech\\Curl\\Request')
-            ->setMethods(array('executeCurlRequest'))
+            ->setMethods(array('executeCurlRequest', 'initializeCurl'))
             ->getMock();
 
         $request->expects($this->once())
             ->method('executeCurlRequest')
-            ->will($this->returnCallback(array($this, 'fakeHttpMessage')));
+            ->will($this->returnCallback(array($this, 'HttpMessageNoRedirect')));
 
         $response = $request->get("http://onlinevind.dk");
 
         $this->assertEquals(200, $response->getCode());
         $this->assertEquals("200 OK", $response->getStatus());
+
         $this->assertCount(8, $response->getHeaders());
-        $this->assertCount(0, $response->getNumberOfRedirects());
+        $this->assertEquals(0, $response->getNumberOfRedirects());
     }
 
-    public function fakeHttpMessage() {
-        return 'HTTP/1.1 200 OK
+    public function HttpMessageNoRedirect() {
+        return <<<FAKEHTTP
+HTTP/1.1 200 OK
 Server: nginx/1.6.2
 Date: Mon, 16 Feb 2015 13:07:27 GMT
 Content-Type: text/html
@@ -122,6 +124,63 @@ Accept-Ranges: bytes
         </div>
     </div>
 </body>
-</html>';
+</html>
+FAKEHTTP;
     }
+
+    public function HttpMessageOneRedirect() {
+    return <<<FAKEHTTP
+HTTP/1.1 303 See other
+Date: Tue, 17 Feb 2015 20:20:48 GMT
+Server: Apache
+X-Powered-By: PHP/5.5.21
+Set-Cookie: 5e8627afafd16a920dac4dbb644715a5=ue1gprimq6rb5r4qsb8sk28jb2; path=/; HttpOnly
+Location: http://care4all.dk/da/
+Connection: close
+Content-Type: text/html; charset=utf-8
+Set-Cookie: SERVERID=; path=/
+
+HTTP/1.1 200 OK
+Date: Tue, 17 Feb 2015 20:20:48 GMT
+Server: Apache
+X-Powered-By: PHP/5.5.21
+Set-Cookie: 76726d50abd3edd601ecfbc19fe61c87=da-DK; path=/
+Set-Cookie: 76726d50abd3edd601ecfbc19fe61c87=da-DK
+P3P: CP="NOI ADM DEV PSAi COM NAV OUR OTRo STP IND DEM"
+Expires: Mon, 1 Jan 2001 00:00:00 GMT
+Last-Modified: Tue, 17 Feb 2015 20:20:48 GMT
+Cache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0
+Pragma: no-cache
+Connection: close
+Content-Type: text/html; charset=utf-8
+Set-Cookie: SERVERID=; path=/
+
+<!doctype html>
+
+<html lang="da">
+<head>
+    <meta charset="utf-8">
+
+    <title>Onlinevind</title>
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <div id="container">
+        <div id="content">
+            <img src="images/onlinevind.png">
+        </div>
+        <div id="footer">
+
+        </div>
+    </div>
+</body>
+</html>
+FAKEHTTP;
 }
+}
+
+//http://pastebin.com/7VVz4jMc
