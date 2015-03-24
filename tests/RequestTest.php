dc5b653cd48abc6f03e4f0a969e3067be2ba7b96
Jeppe Christiansen <jeppe@codr.dk>
2015-03-24 20:52:45 +0100

    Failing test

--- tests/RequestTest.php
+++ tests/RequestTest.php
@@ -18,6 +18,11 @@ class RequestTest extends PHPUnit_Framework_TestCase {
     }
 
     /** @test */
+    public function it_fails() {
+        $this->assertTrue(false);
+    }
+
+    /** @test */
     public function it_can_perform_different_types_of_http_requests() {
 
         $request = $this->getMockBuilder('Jeppech\\Curl\\Request')
